9857f012218675c42db9a8d3ec5bf856f4a50808
arjabusinesscollege <arja_kaura@hotmail.com>
2019-06-13 12:50:31 +0300

    hederi

--- htdocs/wp-content/themes/starter-theme/header.php
+++ htdocs/wp-content/themes/starter-theme/header.php
@@ -6,4 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+   <header>
+       <div class="top-bar">
+           <div class="top-bar-left">
+               <ul>
+                   <a href="#">Etusivu</a>
+                   <a href="http://localhost:8080/blogit">Blogit</a>
+                   <a href="#">Yhteystiedot</a>
+               </ul>
+           </div>
+           <div class="top-bar-right">
+               <ul class="menu">
+                   <li><input type="search" placeholder="Kirjoita tähän..."></li>
+                   <li><button type="button" class="button">Etsi</button></li>
+               </ul>
+           </div>
+       </div>
+   </header>
+   <div class="main-wrapper">
+   <main>
+   

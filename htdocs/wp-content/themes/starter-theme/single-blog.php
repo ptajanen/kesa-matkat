45b5cd7e6e1844d30e7d5da248027b68b3c3ab31
Efe <efe.ogbeide@edu.bc.fi>
2019-06-17 14:23:26 +0300

    Kontakti-sivu ja Efen viimeisin versio 1706

--- htdocs/wp-content/themes/starter-theme/single-blog.php
+++ htdocs/wp-content/themes/starter-theme/single-blog.php
@@ -1,23 +1,49 @@
-
-<?php get_header();?>
-<main>
 <?php
-       if (have_posts()) :
-              while (have_posts()) :
-                                   the_post();
-                     the_title( '<h1>', '</h1>' );
-                     the_post_thumbnail();    
-                     the_content();
+/**
+ * The single template for blog articles.
+ */
 
-              endwhile;
-       endif;
+// Set the global post object for this template.
+the_post();
+
+get_header();
+
 ?>
 
-       <div>
-              <div
-       <?php previous_post_link();?> 
-       <?php next_post_link(); ?>
-
+<main>
+<div class = "container-single-post">  
+	<div class="grid-y grid-padding-x">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-9"> <?php //the latest blog-post?>
+				<div class="grid-x grid-padding-x small-up-2">
+					<div class= "first-post">
+                        <?php       
+                            the_title( '<h1>', '</h1>' );
+                            the_post_thumbnail();    
+                            the_content();
+                        ?>	
+					</div>
+				</div>
+			</div>
+			<div class="cell medium-3"> <?php //sidebar?>
+				<aside class="cell medium-2 accent-bg"> 
+					<?php get_sidebar("blogs"); ?>
+				</aside>     
+            </div> 
+        </div> 
+    </div>    
+</div>
+    <div class = "navigation">
+        <div class = "previous-post">
+            <?php previous_post_link();?> 
+        </div>
+        <div class="cell medium-2">
+            <div class = "next-post">
+                <?php next_post_link(); ?>
+            </div>  
+        </div>	
+    </div>
+                
 <?php get_footer();?>
 
 
--- htdocs/wp-content/themes/starter-theme/src/blogs-functions.php
+++ htdocs/wp-content/themes/starter-theme/src/blogs-functions.php
@@ -14,7 +14,7 @@ function st_register_blog_cpt()
                            'public'      => true,
                            'has_archive' => true,
                            'rewrite'     => array( 'slug' => 'blogit' ), 
-                           'supports'    => array('title', 'editor' , 'excerpt', 'thumbnail'),
+                           'supports'    => array('title', 'editor' , 'excerpt', 'content' ,'thumbnail'),
                            'taxonomies'  => array( 'category', 'post_tag' )
                            
                        )
@@ -23,4 +23,17 @@ function st_register_blog_cpt()
 }
 add_action('init', 'st_register_blog_cpt');
 
-// Register Custom Post Type
+// Jotta kategoriat ja tagit näkyvät --> tämä funktio tuo esiin kaikki alla oleviin kategorioiin ja 
+//tageihin liitetyt postaukset, post, blog, event jne
+
+function tags_categories_support_query($wp_query) {
+ if ($wp_query->get('tag')) {
+   $wp_query->set('post_type', 'any');
+ }
+ if ($wp_query->get('category_name')) {
+   $wp_query->set('post_type', 'any');
+ }
+}
+add_action('pre_get_posts', 'tags_categories_support_query');
+
+// ensure all tags and categories are included in queries
